5225719cd05a38f9eec6ed35b25dde20f9052baf
Tom MacWright <tom@macwright.org>
2020-12-17 09:01:13 -0800

    Use fzf's formatting for better looks

--- podcast-luddite.sh
+++ podcast-luddite.sh
@@ -2,12 +2,13 @@
 
 FEEDS=$(cat $1 | \
   xml sel --net -T -t -m "//*[local-name()='outline'][@xmlUrl]" -v @xmlUrl -o ' ' -v @text -n | \
-  fzf -m)
+  fzf -m --with-nth='2..' --delimiter=' ')
 
 echo $FEEDS | awk '{print $1}' |\
   parallel ./read-rss.sh > .episodes.tmp
 
-cat .episodes.tmp | fzf -m > .picks.tmp
+cat .episodes.tmp | \
+  fzf -m --with-nth='2..' --delimiter=' ' > .picks.tmp
 
 cat .picks.tmp | awk '{print $1}' | sed 's/\\\?.*$//' | sed 's/#.*$//' |\
   xargs aria2c --force-sequential
